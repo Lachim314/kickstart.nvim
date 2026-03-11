b8ae118003981cbaa3fa8de72b1187e016a42a2a
Michal Pekar <michal.pekar@protonmail.com>
2026-03-11 19:28:21 +0100

    Change colorscheme

--- init.lua
+++ init.lua
@@ -123,7 +123,5 @@ require 'lazy-bootstrap'
 
 require 'lazy-plugins'
 
--- vim.cmd [[colorscheme moonfly]]
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
